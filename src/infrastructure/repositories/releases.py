0e3e0cef975f70d8b998447a35c0e4f9af1f9a84
Roman Lee <romanlee1996@gmail.com>
2024-11-10 21:52:00 +0500

    Update

--- src/infrastructure/repositories/releases.py
+++ src/infrastructure/repositories/releases.py
@@ -1,9 +1,23 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
 
 from src.application.interfaces.releases_repository import I_ReleasesRepository
-from src.application.models import Release
+from src.application.models import Release, ReleaseFileMatching
 
 
 class ReleasesRepository(I_ReleasesRepository):
     async def create(self, db_session: AsyncSession, release: Release) -> None:
         db_session.add(release)
+
+    async def get(self, db_session: AsyncSession, name: str) -> Release | None:
+        return await db_session.scalar(
+            select(Release)
+            .where(Release.name == name)
+            .options(joinedload(Release.file_matchings))
+        )
+
+    async def update_file_matchings(
+        self, db_session: AsyncSession, file_matchings: list[ReleaseFileMatching]
+    ) -> None:
+        db_session.add_all(file_matchings)
