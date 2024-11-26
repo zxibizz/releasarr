189b8fef6c388c19922345a758a04f9ecaffd952
Roman Lee <romanlee1996@gmail.com>
2024-11-26 06:10:53 +0000

    Update

--- backend/src/application/utility/release_files_matchings_autocompleter.py
+++ backend/src/application/utility/release_files_matchings_autocompleter.py
@@ -9,6 +9,7 @@ class ReleaseFileMatchingsAutocompleter:
         prev_season_number = None
         prev_episode_number = None
         prev_file_dir = None
+        prev_file_extension = None
 
         sorted_file_matchings: Iterable[ReleaseFileMatching] = sorted(
             file_matchings, key=lambda fm: fm.file_name
@@ -17,10 +18,19 @@ class ReleaseFileMatchingsAutocompleter:
             season_number = file_matching.season_number
             episode_number = file_matching.episode_number
             file_dir = os.path.dirname(file_matching.file_name)
+            file_extension = os.path.splitext(file_matching.file_name)[1]
 
             if prev_file_dir != file_dir:
                 prev_season_number = None
                 prev_episode_number = None
+                prev_file_extension = None
+
+            if (
+                prev_file_extension is not None
+                and file_extension != prev_file_extension
+                and (not season_number and not episode_number)
+            ):
+                continue
 
             if (
                 not season_number
@@ -34,6 +44,8 @@ class ReleaseFileMatchingsAutocompleter:
             file_matching.season_number = season_number
             file_matching.episode_number = episode_number
 
-            prev_season_number = season_number
-            prev_episode_number = episode_number
-            prev_file_dir = file_dir
+            if season_number and episode_number:
+                prev_season_number = season_number
+                prev_episode_number = episode_number
+                prev_file_dir = file_dir
+                prev_file_extension = file_extension
