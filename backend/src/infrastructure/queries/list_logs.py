691bd345025328493661628ec60ef5146e2932cf
Roman Lee <romanlee1996@gmail.com>
2025-02-25 08:21:13 +0000

    Update

--- backend/src/infrastructure/queries/list_logs.py
+++ backend/src/infrastructure/queries/list_logs.py
@@ -6,15 +6,15 @@ import loguru
 from pydantic import BaseModel
 
 
-class ListLogsResponse__Record(BaseModel):
+class DTO_Logs_Record(BaseModel):
     time: str
     level: str
     component: str
     message: str
 
 
-class ListLogsResponse(BaseModel):
-    records: list[ListLogsResponse__Record]
+class DTO_Logs(BaseModel):
+    records: list[DTO_Logs_Record]
 
 
 class Query_ListLogs:
@@ -26,12 +26,12 @@ class Query_ListLogs:
         self.log_file = log_file
         self.logger = logger
 
-    async def execute(self) -> ListLogsResponse:
+    async def execute(self) -> DTO_Logs:
         self.logger.info("List logs")
         with self.logger.catch(reraise=True):
             return await self._execute()
 
-    async def _execute(self) -> ListLogsResponse:
+    async def _execute(self) -> DTO_Logs:
         records = []
         with open(self.log_file, "r") as file:
             lines = file.readlines()[-100:]
@@ -39,11 +39,11 @@ class Query_ListLogs:
             for line in lines:
                 log_entry = json.loads(line)
                 records.append(
-                    ListLogsResponse__Record(
+                    DTO_Logs_Record(
                         time=log_entry["record"]["time"]["repr"],
                         level=log_entry["record"]["level"]["name"],
                         component=log_entry["record"]["extra"]["component"],
                         message=log_entry["text"],
                     )
                 )
-        return ListLogsResponse(records=records)
+        return DTO_Logs(records=records)
