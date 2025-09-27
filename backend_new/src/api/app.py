05c3c2d4373448321f38eb5080c157b9ef6482fa
Roman Lee <romanlee1996@gmail.com>
2025-09-27 14:33:48 +0000

    Allow CORS queries from localhost

--- backend_new/src/api/app.py
+++ backend_new/src/api/app.py
@@ -9,6 +9,7 @@ from __future__ import annotations
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.exceptions import RequestValidationError
 
 from src.api.errors import (
@@ -39,6 +40,23 @@ app = FastAPI(
     lifespan=lifespan,
 )
 """FastAPI ASGI application."""
+
+LOCALHOST_ORIGINS = {
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
+}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=list(LOCALHOST_ORIGINS),
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 register_routes(app)
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
